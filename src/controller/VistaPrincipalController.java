0a8da5d2d162dd278f9e4b34bb84a942849c24be
FHueso <FHueso@Lenovo-FranHA>
2022-02-05 13:13:18 +0100

    Estructura del proyecto creada

---
+++ src/controller/VistaPrincipalController.java
@@ -0,0 +1,24 @@
+/*
+ * Instituo FOC
+ * Grado superior DAM
+ * Proyecto creado para la certificvación Cimientos de Java, incluído en el módulo de programación.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author RECAMBIOS
+ */
+public class VistaPrincipalController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
